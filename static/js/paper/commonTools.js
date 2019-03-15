e97b65f48315da0e8378ddeda53136da0214927f
LiZeC <tomgreem123@gmail.com>
2019-03-15 09:44:07 +0800

    添加对负数的处理

--- static/js/paper/commonTools.js
+++ static/js/paper/commonTools.js
@@ -1,22 +1,46 @@
 // 注意： 所有的PaperScript都依赖此文件，因此需要将此文件先于所有PaperScript引入
 // 此文件需要以PaperScript的方式导入，并且与任意的Canvas绑定
+function matrixBorder(matrix) {
+    var max = matrix[0][0];
+    var min = matrix[0][0];
+    matrix.forEach(function (arr) {
+        arr.forEach(function (ele) {
+            if (ele < min) {
+                min = ele;
+            } else if (ele > max) {
+                max = ele;
+            }
+        });
+    });
+    return [min, max];
+}
+
+
 window.drawMatrixNerve = function (point, margin, radius, data) {
+    var b = matrixBorder(data);
+    var min = b[0];
+    var max = b[1];
+    var scan = max - min;
+
     var width = data[0].length;
     var height = data.length;
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
             var c = Shape.Circle(point.x + 2 * x * margin + margin, point.y + 2 * y * margin + margin, radius);
-            // 绝对值越大，透明度越低
-            // 对于边框颜色, 正数为白色,负数为黑色
-
-            if (data[y][x] >= 0) {
+            if (min >= 0) {
+                // 只有正数的场景，全部使用白色，值越大，透明度越低，颜色越白
                 c.strokeColor = 'white';
-                // c.strokeColor = new Color(1, data[y][x] / 256.0);
-                c.fillColor = new Color(1, data[y][x] / 256.0);
+                c.fillColor = new Color(1, data[y][x] / scan);
             } else {
-                //console.log(data[y][x]);
-                c.strokeColor = 'black';
-                c.fillColor = new Color(1, -data[y][x] / 256.0);
+                // 正负数同时存在的场景，分别使用黑白两种颜色
+                // 绝对值越大，透明度越低
+                if (data[y][x] >= 0) {
+                    c.strokeColor = 'white';
+                    c.fillColor = new Color(1, data[y][x] / 256.0);
+                } else {
+                    c.strokeColor = 'black';
+                    c.fillColor = new Color(0, -data[y][x] / 256.0);
+                }
             }
         }
     }
