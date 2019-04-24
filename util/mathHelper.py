ecf570b54436b062692d4fd3bde0f4b5ea19fbdc
LiZeC <tomgreem123@gmail.com>
2019-04-24 17:09:12 +0800

    更新坐标计算公式，修复关于边框线宽的计算错误

--- util/mathHelper.py
+++ util/mathHelper.py
@@ -1,13 +1,39 @@
 def calculateImageLocation(length, cutNum, margin, num):
-    imageHeight = margin * num * 2 + 8
+    """
+    :param length:  绘图区域的总长度
+    :param cutNum: 需要分割的数量，即需要绘制的图的数量
+    :param margin: 每一个图中各个神经元的间距
+    :param num: 每一个图中每一列包含的神经元数量
+    :return: 输入分割方案的起始距离和间距
+    """
+    # 图像的整体长度等于margin+(margin*(num-1))+margin+L/2+L/2
+    # 其中L为边框线宽
+    imageHeight = (2 * margin * num) + 4
     if length < imageHeight * cutNum:
         print(f"长度小于最低要求，问题无解(至少需要{imageHeight * cutNum})")
     else:
         interval = length / cutNum
-        begin = (interval / 2) - (imageHeight / 2)
+        # 绘图点是边框线交叉部分的中点
+        # 图像最两端都有半个线宽的距离
+        #  |----------
+        #  |  x  |
+        #  |-----|----
+        #  |     |
+        begin = (interval / 2) - (imageHeight / 2) + 2
         print(f"起始距离为{begin},间距为{interval}")
 
 
 if __name__ == '__main__':
+    print("输入层")
+    calculateImageLocation(1600, 1, 6, 28)
+    print("第一卷积层和ReLU层")
+    calculateImageLocation(1600, 4, 6, 28)
+    print("第一池化层")
+    calculateImageLocation(1600, 4, 9, 14)
+    print("第二卷积层和ReLU层")
     calculateImageLocation(1600, 8, 6, 14)
-    # calculateImageLocation(1000,4,1,45)
+    print("第二池化层")
+    calculateImageLocation(1600, 8, 9, 7)
+    print("全连接层")
+    calculateImageLocation(1600, 16, 20, 1)
+
