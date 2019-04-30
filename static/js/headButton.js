2e804d52bb24bf1b46e10ff9864749fb47d7fc03
LiZeC <tomgreem123@gmail.com>
2019-04-30 16:42:15 +0800

    添加播放控制功能

--- static/js/headButton.js
+++ static/js/headButton.js
@@ -9,7 +9,7 @@ function drawExport(URL) {
     dataTool.upload({"image": img_png_src}, function (data) {
         window.calcData = data;
         const name = URL.split('/').pop();
-         window.dialog.hide();
+        window.dialog.hide();
         if (name === 'main.html') {
             window.mainMainUpdate();
             window.conv1ConvUpdate();
@@ -46,22 +46,6 @@ function trainExport() {
     })
 }
 
-let trainFlag = false;
-
-function trainGradient() {
-    trainFlag = !trainFlag;
-    if (trainFlag) {
-        window.conv1WbUpdate(trainData.gradient.conv1, "gradient");
-        window.conv2WbUpdate(trainData.gradient.conv2, "gradient");
-        window.mainFcUpdate(trainData, "gradient");
-    } else {
-        window.conv1WbUpdate(trainData.first.conv1, "data");
-        window.conv2WbUpdate(trainData.first.conv2, "data");
-        window.mainFcUpdate(trainData, "data");
-    }
-
-}
-
 function drawNew() {
     $.post(config.baseURL + "/getMnistImage", function (jsonStr) {
         var data = JSON.parse(jsonStr);
@@ -71,6 +55,7 @@ function drawNew() {
     })
 }
 
+let trainFlag = false;
 $(document).keydown(function (event) {
     if (trainData !== undefined) {
         if (event.key === 'a') {
@@ -92,36 +77,52 @@ $(document).keydown(function (event) {
 
 });
 
+let intervalID;
+let dataIdx = 0;
+let isPause = true;
 
-function drawBegin() {
-    dataTool.trainData({}, function (result) {
-        window.trainData = {};
-        console.log(result);
-        trainFlag = true;
-        let dataIdx = 0;
-        // 第一项是数据，第二项是梯度
-        setInterval(function () {
-            // 越界保护的逻辑
-            if (dataIdx >= result.length) {
-                return;
-            }
+function drawHandler() {
+    // 如果达到数组的最后一个元素 或者 当前处于暂停状态
+    if (dataIdx >= trainDataList.length) {
+        return;
+    }
 
-            if (dataIdx % 2 === 0) {
-                //console.log("Draw Data,dataIdx=", dataIdx);
-                //console.log(result[dataIdx].conv1.W[0][0][0]);
-                trainData.first = result[dataIdx];
-                window.conv1WbUpdate(trainData.first.conv1, "data");
-                window.conv2WbUpdate(trainData.first.conv2, "data");
-                window.mainFcUpdate(trainData, "data");
-            } else {
-                //console.log("Draw Gradient,dataIdx=", dataIdx);
-                //console.log(result[dataIdx]);
-                trainData.gradient = result[dataIdx];
-                window.conv1WbUpdate(trainData.gradient.conv1, "gradient");
-                window.conv2WbUpdate(trainData.gradient.conv2, "gradient");
-                window.mainFcUpdate(trainData, "gradient");
-            }
-            dataIdx++;
-        }, 200);
-    })
+    if (dataIdx % 2 === 0) {
+        trainData.first = trainDataList[dataIdx];
+        window.conv1WbUpdate(trainData.first.conv1, "data");
+        window.conv2WbUpdate(trainData.first.conv2, "data");
+        window.mainFcUpdate(trainData, "data");
+    } else {
+        trainData.gradient = trainDataList[dataIdx];
+        window.conv1WbUpdate(trainData.gradient.conv1, "gradient");
+        window.conv2WbUpdate(trainData.gradient.conv2, "gradient");
+        window.mainFcUpdate(trainData, "gradient");
+    }
+    dataIdx++;
+}
+
+function trainBack() {
+    clearInterval(intervalID);
+    // dataIdx指向下一个需要绘制的位置，因此回退需要-2
+    dataIdx = dataIdx - 2;
+    drawHandler();
+    console.log(dataIdx);
+}
+
+function trainForward() {
+    clearInterval(intervalID);
+    drawHandler();
+    console.log(dataIdx);
+}
+
+function trainPause() {
+    if (isPause) {
+        intervalID = setInterval(drawHandler, 200);
+        $("#btnPause").text("暂停")
+    } else {
+        console.log(dataIdx);
+        clearInterval(intervalID);
+        $("#btnPause").text("恢复")
+    }
+    isPause = !isPause;
 }
